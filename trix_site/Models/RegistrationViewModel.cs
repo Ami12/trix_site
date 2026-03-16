d1ef000790013adc3ed3314b5de4671944c1ef45
Ami12 <ami@12trux.com>
2026-03-16 23:28:21 +0200

    NEW SITE Simple 960 Shekel Noy for everybody

---
+++ trix_site/Models/RegistrationViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace trix_site.Models
+{
+    public class RegistrationViewModel
+    {
+        [Required]
+        public bool TermsAccepted { get; set; }
+
+        [Required]
+        public bool ParentCommitment { get; set; }
+    }
+}
